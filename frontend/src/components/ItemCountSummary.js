80b5d8f0a5834b8ffd7568fbfdde7418633e055c
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-01-29 19:16:15 +0530

    Update ItemCountSummary.js

--- frontend/src/components/ItemCountSummary.js
+++ frontend/src/components/ItemCountSummary.js
@@ -6,7 +6,7 @@ const ItemCountSummary = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/items')
+    axios.get('http://18.228.13.1:5000/api/items')
       .then(response => {
         setItems(response.data);
         setLoading(false);
